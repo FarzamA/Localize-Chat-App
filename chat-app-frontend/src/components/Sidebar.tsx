c2cfe0d23122e6d8538ac42c0752bae1d73b46a8
Farzam Ahmad <farzam135@gmail.com>
2024-10-23 05:40:53 -0400

    add modification in order to make sidebar look more user friendly

--- chat-app-frontend/src/components/Sidebar.tsx
+++ chat-app-frontend/src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 // src/components/Sidebar.tsx
 import React from 'react';
-import { Drawer, List, ListItem, Typography, Button } from '@mui/material';
+import { Drawer, List, ListItem, Typography, Divider, Button, Box } from '@mui/material';
 
 interface SidebarProps {
   open: boolean;
@@ -8,13 +8,36 @@ interface SidebarProps {
 }
 
 export const Sidebar: React.FC<SidebarProps> = ({ open, onClose }) => {
+  // Dummy rooms list can be modified later for better functionality
+  const rooms = ['Room 1'];
+
   return (
     <Drawer open={open} onClose={onClose}>
-      <List>
-        <Button >
-          <Typography variant="h6">Room 1</Typography>
-        </Button>
-      </List>
+      <Box sx={{ width: 250, p: 2 }}>
+        {/* Heading */}
+        <Typography variant="h6" sx={{ textAlign: 'center' }}>
+          Chat Rooms
+        </Typography>
+
+        {/* Divider */}
+        <Divider sx={{ my: 2 }} />
+
+        {/* List of Rooms */}
+        <List>
+          {rooms.map((room, index) => (
+            <ListItem key={index} disablePadding>
+              <Button
+                fullWidth
+                variant="text"
+                sx={{ justifyContent: 'flex-start', textTransform: 'none' }}
+                onClick={() => console.log(`Selected: ${room}`)}  // Replace with actual room selection logic
+              >
+                {room}
+              </Button>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
     </Drawer>
   );
 };
